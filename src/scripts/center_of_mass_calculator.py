07024145e3bece09d0c5414e2060fe2f59de8127
Evan Wyse <evan.wyse@duke.edu>
2020-07-31 10:47:13 -0700

    Fixes to com calculator

--- src/scripts/center_of_mass_calculator.py
+++ src/scripts/center_of_mass_calculator.py
@@ -4,13 +4,13 @@ import pickle
 import pandas as pd
 import os
 
+sys.path.append('/gtmp/etw16/nonreversiblecodebase/')
+
 from src.analytics import extract_center_of_mass
 
 
-sys.path.append('/gtmp/etw16/nonreversiblecodebase/')
-
-
 df = pd.read_csv(sys.argv[1])
+out_folder = sys.argv[2]
 
 for fi in list(df.filepath):
 
@@ -24,6 +24,6 @@ for fi in list(df.filepath):
     out_path = fp.replace('.pkl', '.json')
     com = extract_center_of_mass(process)
 
-    with open(fp, mode='w') as f:
+    with open(os.path.join(out_folder, fp), mode='w') as f:
         json.dump(com)
 
