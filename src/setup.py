63b7ef1ad36159b93de6027ca97ead46112ba632
Evan Wyse <evan.wyse@duke.edu>
2020-08-05 10:58:12 -0700

    Adding package info

--- src/setup.py
+++ src/setup.py
@@ -2,10 +2,21 @@ from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Build import cythonize
 from Cython.Distutils import build_ext
+from setuptools import find_packages
 
 ext_modules = [Extension("biconnected", ['biconnected.pyx'], language="c++", extra_compile_args=['-std=c++11'])]
 
 
-setup(ext_modules=ext_modules, cmdclass={"build_ext":build_ext})
+setup(
+    name="",
+    version="0.0.1",
+    author= "Evan Wyse",
+    author_email="evan.wyse@duke.edu",
+    description="Non-reversible MCMC chain sampling over districting graphs",
+    packages=find_packages(),
+    ext_modules=ext_modules, cmdclass={"build_ext":build_ext},
+    python_requires='>=3.5',
+
+    )
 
 # TODO we may need to
