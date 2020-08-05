646d88c97668afe8b66f6bc3e372498f268e248a
Evan Wyse <evan.wyse@duke.edu>
2020-08-05 16:53:18 -0400

    Local changes

--- src/setup.py
+++ src/setup.py
@@ -1,19 +1,19 @@
+import setuptools
 from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Build import cythonize
 from Cython.Distutils import build_ext
-from setuptools import find_packages
 
-ext_modules = [Extension("biconnected", ['biconnected.pyx'], language="c++", extra_compile_args=['-std=c++11'])]
+ext_modules = [Extension("biconnected", ['src/biconnected.pyx'], language="c++", extra_compile_args=['-std=c++11'])]
 
 
 setup(
-    name="",
+    name="packagemcpackageface",
     version="0.0.1",
     author= "Evan Wyse",
     author_email="evan.wyse@duke.edu",
     description="Non-reversible MCMC chain sampling over districting graphs",
-    packages=find_packages(),
+    packages=[],
     ext_modules=ext_modules, cmdclass={"build_ext":build_ext},
     python_requires='>=3.5',
 
