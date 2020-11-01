fd8aca3479054e1de6998429f29b62920846f9db
Evan Wyse <evan.wyse@duke.edu>
2020-11-01 12:24:47 -0500

    Updates

--- src/nrmc/core.py
+++ src/nrmc/core.py
@@ -36,7 +36,7 @@ score_lookup = {'cut_length': cut_length_score,
 score_updaters = {'cut_length': [],
                   'compactness': [update_perimeter_and_area],
                   'population_balance': [update_population],
-                  'gsl': update_matrix
+                  'gsl': [update_matrix]
   }
 
 class ProcessEncoder(json.JSONEncoder):
