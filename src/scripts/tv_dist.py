3528d0a4866b9f4314a2b90abf97ca335d26fc2b
Evan Wyse <evan.wyse@duke.edu>
2021-02-15 14:26:42 -0500

    Bugfix

--- src/scripts/tv_dist.py
+++ src/scripts/tv_dist.py
@@ -31,7 +31,7 @@ def collect_var(filepath_csv, states=6, overwrite=False):
         df_fi = pd.read_csv(fi_wins)
         my_list.append(df_fi)
     # print('pang')
-    df_all = pd.concat(my_list, axis=1) # TODO check this
+    df_all = pd.concat(my_list, axis=1).dropna() # TODO check this
     # TODO drop extra rows - truncate to a maximum value
 
 
@@ -61,7 +61,6 @@ def collect_var(filepath_csv, states=6, overwrite=False):
     df_out.to_csv(out_path, index=None)
 
 
-print(args.filepaths)
 
 func = functools.partial(collect_var, overwrite=overwrite)
 
