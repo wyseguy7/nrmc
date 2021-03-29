73fff6bb64f93083b04b1a3de460e57aaad2fc80
Evan Wyse <evan.wyse@duke.edu>
2021-03-29 14:24:33 -0400

    Improved logging

--- src/scripts/tv_dist_new.py
+++ src/scripts/tv_dist_new.py
@@ -6,6 +6,7 @@ import numpy as np
 from collections import Counter, defaultdict
 
 import pandas as pd
+import time
 
 parser = argparse.ArgumentParser()
 # NOTE different behavior here - filepaths is a LIST of filepaths
@@ -20,7 +21,9 @@ def quantize(x):
     return round(2*x, 2) # round to half-percentage point
 
 
-def collect_var(filepath_csv, states=6, overwrite=False):
+def collect_var(filepath_csv, overwrite=False):
+
+    checkpoint = time.time()
 
     df_filepaths = pd.read_csv(filepath_csv)
     fo, fi = os.path.split(filepath_csv)
@@ -34,6 +37,9 @@ def collect_var(filepath_csv, states=6, overwrite=False):
         df_fi = pd.read_csv(fi_wins)
         my_list.append(df_fi)
     # print('pang')
+    print("Finished loading data in {:.2f} seconds ".format(time.time()-checkpoint))
+    checkpoint = time.time()
+
 
     num_districts = len(my_list[0].columns)
 
@@ -45,10 +51,12 @@ def collect_var(filepath_csv, states=6, overwrite=False):
         district_list.append(df)
 
     # now, we have quantized picture of each
-    print("ping")
+
     pibar_total = [Counter(np.ravel(df.values)) for df in district_list] # make this work
     pibar_list = [{k:v/sum(d.values()) for k,v in d.items()} for d in pibar_total] # normalize by counts
-    print("pong")
+
+    print("Finished calculating pibar in {:.2f} seconds ".format(time.time()-checkpoint))
+    checkpoint = time.time()
     K = len(my_list) # number of chains
     N = len(my_list[0])
     tv_dist = np.zeros(shape=(N, K, num_districts))
@@ -68,7 +76,8 @@ def collect_var(filepath_csv, states=6, overwrite=False):
                 tv_dist[i,k,district_idx] = 0.5*np.sum([abs(hist_list[k][key]-v) for key, v in pibar.items()])/(i+1)
 
             if i % 1000000 == 0:
-                print(i)
+                print("Finished calculating 1000000 iterations in {:.2f} seconds ".format(time.time() - checkpoint))
+                checkpoint = time.time()
 
     tv_dist_col = tv_dist.mean(axis=2) # take the mean over tv distance for each
     df_out = pd.DataFrame(tv_dist_col)
